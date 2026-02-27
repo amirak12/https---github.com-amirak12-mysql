3315aba55e16cc16e108320548c945b4d255115c
AmirKilerh14 <diwgole72@gmail.com>
2026-02-27 22:58:32 +0330

    updated drop

--- codes.sql
+++ codes.sql
@@ -1,6 +1,7 @@
 mysql commands? dorod to salmasi
 CREATE DATABASE mydb;
 DROP DATABASE mydb;
+DROP TABLE mytable;
 CREATE TABLE mytable(id INT() AUTO_INCREMENT ,name VARCHAR(50),uuid int(40),PRIMARY KEY(id));
 ALTER TABLE mytable ADD COLUMN myfield INT(30);
 ALTER TABLE mytable REMOVE COLUMN myfield INT(30);
@@ -9,4 +10,5 @@ UPDATE SET `uuid` = 24 WHERE `id` = 1 ;
 UPDATE SET `uuid` = 24 WHERE `id` BETWEEN 1 AND 10;
 DELETE * FROM mytable WHERE `id` = 1;
 DELETE * FROM mytable WHERE `id` BETWEEN 1 AND 10;
+
   
