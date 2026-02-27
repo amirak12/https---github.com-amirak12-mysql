a4d7c5275a728a0d1ac84d06397490b317a7c545
AmirKilerh14 <diwgole72@gmail.com>
2026-02-27 22:30:26 +0330

    added mysql

---
+++ codes.sql
@@ -0,0 +1,10 @@
+mysql commands? kiram to salmasi koskesh
+
+CREATE DATABASE mydb;
+
+DROP DATABASE mydb;
+CREATE TABLE mytable(id INT() AUTO_INCREMENT ,name VARCHAR(50),uuid int(40),PRIMARY KEY(id))
+
+ALTER TABLE mytable ADD COLUMN myfield INT(30)
+ALTER TABLE mytable REMOVE COLUMN myfield INT(30)
+INSERT INTO TABLENAME(`name`,`uuid`) VALUES("kos",12312),("mamad",1324);
