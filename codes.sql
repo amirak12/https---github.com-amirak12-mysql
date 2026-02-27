744166a910128c69584b5dcf7b7c90f04d435253
AmirKilerh14 <diwgole72@gmail.com>
2026-02-27 22:56:52 +0330

    added delete

--- codes.sql
+++ codes.sql
@@ -7,4 +7,6 @@ ALTER TABLE mytable REMOVE COLUMN myfield INT(30);
 INSERT INTO TABLENAME(`name`,`uuid`) VALUES("salam",12312),("ali",1324);
 UPDATE SET `uuid` = 24 WHERE `id` = 1 ;
 UPDATE SET `uuid` = 24 WHERE `id` BETWEEN 1 AND 10;
+DELETE * FROM mytable WHERE `id` = 1;
+DELETE * FROM mytable WHERE `id` BETWEEN 1 AND 10;
   
