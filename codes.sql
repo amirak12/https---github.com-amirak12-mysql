1e35d850429dfbdba210a3dfa1c2dea76fc693eb
AmirKilerh14 <diwgole72@gmail.com>
2026-02-28 00:00:53 +0330

    this is the last i promise

--- codes.sql
+++ codes.sql
@@ -4,7 +4,7 @@ DROP DATABASE mydb;
 DROP TABLE mytable;
 CREATE TABLE mytable(id INT() AUTO_INCREMENT ,name VARCHAR(50),uuid int(40),PRIMARY KEY(id));
 ALTER TABLE mytable ADD COLUMN myfield INT(30);
-ALTER TABLE mytable REMOVE COLUMN myfield INT(30);
+ALTER TABLE mytable DROP COLUMN myfield INT(30);
 INSERT INTO TABLENAME(`name`,`uuid`) VALUES("salam",12312),("ali",1324);
 UPDATE SET `uuid` = 24 WHERE `id` = 1 ;
 UPDATE SET `uuid` = 24 WHERE `id` BETWEEN 1 AND 10;
