41f0dd052a72f06c52e5a8fd6f969a4df5efd6a7
AmirKilerh14 <diwgole72@gmail.com>
2026-02-27 22:54:59 +0330

    added update

--- codes.sql
+++ codes.sql
@@ -5,4 +5,6 @@ CREATE TABLE mytable(id INT() AUTO_INCREMENT ,name VARCHAR(50),uuid int(40),PRIM
 ALTER TABLE mytable ADD COLUMN myfield INT(30);
 ALTER TABLE mytable REMOVE COLUMN myfield INT(30);
 INSERT INTO TABLENAME(`name`,`uuid`) VALUES("salam",12312),("ali",1324);
-UPDATE SET `uuid` = 24 WHERE `id` = 1;
+UPDATE SET `uuid` = 24 WHERE `id` = 1 ;
+UPDATE SET `uuid` = 24 WHERE `id` BETWEEN 1 AND 10;
+  
